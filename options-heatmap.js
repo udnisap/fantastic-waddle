add0d3f9a7254831aa9b728be76cebaebac24637
Pasindu Perera <udnisap@users.noreply.github.com>
2020-05-03 15:37:41 -0700

    options heatmap

---
+++ options-heatmap.js
@@ -0,0 +1,177 @@
+// const calls = [{"putCall":"CALL","symbol":"SPY_050420C130","description":"SPY May 4 2020 130 Call (Weekly)","exchangeName":"OPR","bid":152.25,"ask":152.73,"last":0,"mark":152.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":152.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":152.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":130,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C135","description":"SPY May 4 2020 135 Call (Weekly)","exchangeName":"OPR","bid":147.25,"ask":147.73,"last":0,"mark":147.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":147.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":147.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":135,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C140","description":"SPY May 4 2020 140 Call (Weekly)","exchangeName":"OPR","bid":142.25,"ask":142.73,"last":0,"mark":142.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":142.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":142.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":140,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C145","description":"SPY May 4 2020 145 Call (Weekly)","exchangeName":"OPR","bid":137.25,"ask":137.73,"last":0,"mark":137.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":137.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":137.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":145,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C150","description":"SPY May 4 2020 150 Call (Weekly)","exchangeName":"OPR","bid":132.25,"ask":132.73,"last":0,"mark":132.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":132.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":132.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":150,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C155","description":"SPY May 4 2020 155 Call (Weekly)","exchangeName":"OPR","bid":127.25,"ask":127.73,"last":0,"mark":127.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":127.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":127.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":155,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C160","description":"SPY May 4 2020 160 Call (Weekly)","exchangeName":"OPR","bid":122.25,"ask":122.73,"last":0,"mark":122.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":122.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.151,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":122.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":160,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C165","description":"SPY May 4 2020 165 Call (Weekly)","exchangeName":"OPR","bid":117.25,"ask":117.73,"last":0,"mark":117.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":117.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.15,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":117.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":165,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C170","description":"SPY May 4 2020 170 Call (Weekly)","exchangeName":"OPR","bid":112.25,"ask":112.73,"last":0,"mark":112.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":112.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.15,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":112.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":170,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false},{"putCall":"CALL","symbol":"SPY_050420C175","description":"SPY May 4 2020 175 Call (Weekly)","exchangeName":"OPR","bid":107.25,"ask":107.73,"last":0,"mark":107.49,"bidSize":1,"askSize":1,"bidAskSize":"1X1","lastSize":0,"highPrice":0,"lowPrice":0,"openPrice":0,"closePrice":107.49,"totalVolume":0,"tradeDate":null,"tradeTimeInLong":0,"quoteTimeInLong":1588364099900,"netChange":0,"volatility":5,"delta":1,"gamma":0,"theta":0,"vega":0.15,"rho":0,"openInterest":0,"timeValue":-0.3,"theoreticalOptionValue":107.92,"theoreticalVolatility":29,"optionDeliverablesList":null,"strikePrice":175,"expirationDate":"May 4th 20","daysToExpiration":1,"expirationType":"S","lastTradingDay":1588636800000,"multiplier":100,"settlementType":" ","deliverableNote":"","isIndexOption":null,"percentChange":0,"markChange":0,"markPercentChange":0,"mini":false,"inTheMoney":true,"nonStandard":false}];
+const objsToMatrix = data => {
+  const [matE, matS] = data.reduce(([mapE, mapS], d) =>{
+    const s = d.x;
+    const e = d.y;
+    mapE[e] = mapE[e] || {};
+    mapE[e][s] = d;
+    mapS[s] = mapS[s] || {};
+    mapS[s][e] = d;
+    return [mapE, mapS];
+  }, [{}, {}]);
+  const sVals = new Set(data.map(s => s.x))
+  const eVals = new Set(data.map(s => s.y));
+  const output = [];
+  for(let sVal of sVals){
+    for(let eVal of eVals){
+      if (matE[eVal][sVal]) {
+        output.push([eVal, sVal, matE[eVal][sVal].z]);
+      } else {
+        const estimate1 = calculateEstimate(
+          matE[eVal],
+          Object.values(matE[eVal]).map(d => d.x),
+          sVal
+        );
+        const estimate2 = calculateEstimate(
+          matS[sVal],
+          Object.values(matS[sVal]).map(d => d.y),
+          eVal
+        );
+        output.push([eVal, sVal, Math.max(estimate1, estimate2)]);
+      }
+    }
+  }
+  return output;
+}
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 80, right: 25, bottom: 30, left: 40},
+  width = window.innerWidth - margin.left - margin.right,
+  height = 5* window.innerHeight - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+fetch( "https://api.tdameritrade.com/v1/marketdata/chains?apikey=DUK17TRKRULCUWO0DLQZ0KI2OUYBHY6H&symbol=SPY&contractType=CALL", {
+  headers: { 'Authorization': ''}
+})
+  .then(data => data.json())
+  .then(data => [data.callExpDateMap || [], data.putExpDateMap || []])
+  .then(([calls, puts]) => {
+// const data =[
+// {group: "A", variable: "v1", value: "30"},
+// {group: "B", variable: "v2", value: "95"}
+// ]
+const data = Object.values(calls)
+      .flatMap(c => Object.values(c).map(d => d[0]))
+      .map(s => ({
+        // ...s,
+        value: s.bid,
+				strikePrice: parseFloat(s.strikePrice),
+        // expirationDate: moment(s.expirationDate).format("MMM Do YY")
+        expirationDate: s.daysToExpiration
+      }))
+    // .slice(0, 10)
+//Read the data
+  // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
+    const expirations = Array.from(new Set(data.map(s => s.expirationDate)));
+    const strikes = Array.from(new Set(data.map(s => s.strikePrice))).sort((a, b) => a -b)
+  // var myGroups = d3.map(callExpDateMap, function(d){return d.group;}).keys()
+  // var myVars = d3.map(data, function(d){return d.variable;}).keys()
+
+  // Build X scales and axis:
+  var x = d3.scaleBand()
+    .range([ 0, width ])
+    .domain(expirations)
+    .padding(0.05);
+  svg.append("g")
+    .style("font-size", 15)
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSize(0))
+    .select(".domain").remove()
+
+  // Build Y scales and axis:
+  var y = d3.scaleBand()
+    .range([ height, 0 ])
+    .domain(strikes)
+    .padding(0.05);
+
+  svg.append("g")
+    .style("font-size", 15)
+    .call(d3.axisLeft(y).tickSize(0))
+    .select(".domain").remove()
+
+  // Build color scale
+  var myColor = d3.scaleSequential()
+    .interpolator(d3.interpolateInferno)
+    .domain([1,100])
+
+  // create a tooltip
+  var tooltip = d3.select("#my_dataviz")
+    .append("div")
+    .style("opacity", 0)
+    .attr("class", "tooltip")
+    .style("background-color", "white")
+    .style("border", "solid")
+    .style("border-width", "2px")
+    .style("position", "absolute")
+    .style("border-radius", "5px")
+    .style("padding", "5px")
+
+  // Three function that change the tooltip when user hover / move / leave a cell
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1)
+    d3.select(this)
+      .style("stroke", "black")
+      .style("opacity", 1)
+  }
+  var mousemove = function(d) {
+    tooltip
+      .html(`Strike: ${d.strikePrice} <br/> Expiration: ${d.expirationDate} <br/> Value: ${d.value}`)
+      .style("left", (d3.mouse(this)[0]+70) + "px")
+      .style("top", (d3.mouse(this)[1]) + "px")
+  }
+  var mouseleave = function(d) {
+    tooltip
+      .style("opacity", 0)
+    d3.select(this)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+  }
+
+  // add the squares
+  svg.selectAll()
+    .data(data, function(d) {return d.expirationDate+':'+d.strikePrice;})
+    .enter()
+    .append("rect")
+      .attr("x", function(d) { return x(d.expirationDate) })
+      .attr("y", function(d) { return y(d.strikePrice) })
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .attr("width", x.bandwidth() )
+      .attr("height", y.bandwidth() )
+      .style("fill", function(d) { return myColor(d.value)} )
+      .style("stroke-width", 4)
+      .style("stroke", "none")
+      .style("opacity", 0.8)
+    .on("mouseover", mouseover)
+    .on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
+})
+
+// // Add title to graph
+// svg.append("text")
+//         .attr("x", 0)
+//         .attr("y", -50)
+//         .attr("text-anchor", "left")
+//         .style("font-size", "22px")
+//         .text("A d3.js heatmap");
+//
+// // Add subtitle to graph
+// svg.append("text")
+//         .attr("x", 0)
+//         .attr("y", -20)
+//         .attr("text-anchor", "left")
+//         .style("font-size", "14px")
+//         .style("fill", "grey")
+//         .style("max-width", 400)
+//         .text("A short description of the take-away message of this chart.");
+//
